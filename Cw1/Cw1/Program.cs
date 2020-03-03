16891027308a6b8c0ae8cc8dcfe8c236817a6c2e
Patryk Grzeszczak <s19676@pjwstk.edu.pl>
2020-03-03 09:54:16 +0100

    Pierwszy commit

---
+++ Cw1/Cw1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Cw1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+
+            var url = Console.ReadLine();
+
+            var httpClient = new HttpClient();
+            var content = await httpClient.GetStringAsync(url);
+            //Console.WriteLine(content);
+
+            var list = ExtractEmails(content);
+
+            string result = string.Join(",", ExtractEmails(content));
+            Console.WriteLine(result);
+
+            //foreach(string s in list)
+            //{
+               // Console.WriteLine(content);
+            //}
+        }
+        public static string[] ExtractEmails(string str)
+        {
+            string RegexPattern = @"\b[A-Z0-9._-]+@[A-Z0-9][A-Z0-9.-]{0,61}[A-Z0-9]\.[A-Z.]{2,6}\b";
+
+            // Find matches
+            System.Text.RegularExpressions.MatchCollection matches
+                = System.Text.RegularExpressions.Regex.Matches(str, RegexPattern, System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+            string[] MatchList = new string[matches.Count];
+
+            // add each match
+            int c = 0;
+            foreach (System.Text.RegularExpressions.Match match in matches)
+            {
+                MatchList[c] = match.ToString();
+                c++;
+            }
+
+            return MatchList;
+        }
+    }
+}
